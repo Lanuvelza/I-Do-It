1fa75639c2f95c8d9599bdbfcd2f244b88030094
Taylor <ivingstaylor@gmail.com>
2021-02-09 18:46:06 +0000

    api routes return object with values for each api

--- routes/api.js
+++ routes/api.js
@@ -1,42 +1,47 @@
-const request = require('request');
 require('dotenv').config()
 const fetch = require("node-fetch");
-const { response } = require('express');
 
-const amountOfResults = (userInput) => {
+let books;
+let recipes;
+let movies;
 
-async  function fetchBookResults() {
-    const fetchBook = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${userInput}+intitle:${userInput}`)
-    const totalResult = await fetchBook.json()
-    return totalResult.totalItems/75;
-  }
-async  function fetchRecipeResults() {
-    const fetchRecipe = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${userInput}&apiKey=${process.env.FOOD_API_KEY}`)
-    const totalResult = await fetchRecipe.json()
-    return totalResult.totalResults;
-  }
-async  function fetchMovieResults() {
-    const fetchBook = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${userInput}`)
-    const totalResult = await fetchBook.json()
-    return totalResult.total_results
-  }
-  const arr = [fetchBookResults(), fetchRecipeResults(), fetchMovieResults()]
-  let obj = {
-  }
-  Promise.all(arr).then((values) => {
-    return values;
-  })
-  .then((values) => {
-obj = {
-  book: values[0],
-  recipe: values[1],
-  movie: values[2]
+let obj = {}
+
+async function fetchBookResults(UI) {
+  const fetchBook = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${UI}+intitle:${UI}`)
+  const totalResult = await fetchBook.json()
+  return totalResult.totalItems/75;
 }
-})
+async function fetchRecipeResults(UI) {
+  const fetchRecipe = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${UI}&apiKey=${process.env.FOOD_API_KEY}`)
+  const totalResult = await fetchRecipe.json()
+  return totalResult.totalResults;
+}
+async function fetchMovieResults(UI) {
+  const fetchBook = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${UI}`)
+  const totalResult = await fetchBook.json()
+  return totalResult.total_results
+}
 
-  return obj;
-};
 
-console.log(amountOfResults("toy story"));
+
+async function amountOfResults (userInput)  {
+
+books = await fetchBookResults(userInput)
+recipes = await fetchRecipeResults(userInput)
+movies = await fetchMovieResults(userInput)
+setTimeout(()=> {console.log(books)
+  obj = {
+    books,
+    recipes,
+    movies
+  }
+
+  console.log(obj)
+  }, 2000)
+
+}
+amountOfResults("pasta")
+
 
  module.exports = amountOfResults;
