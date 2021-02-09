8f630caf40161b2b6692dd07b8acaf5c1c6ded8f
Taylor <ivingstaylor@gmail.com>
2021-02-09 06:23:36 +0000

    fetch api results working, making them an object is not

--- routes/api.js
+++ routes/api.js
@@ -1,43 +1,42 @@
 const request = require('request');
 require('dotenv').config()
+const fetch = require("node-fetch");
+const { response } = require('express');
 
-const test = process.argv.splice(2).join("+");
-const  fetchRecipe = function(callback) {
-  request(`https://api.spoonacular.com/recipes/complexSearch?query=${test}&apiKey=${process.env.API_KEY}`, (error, response, body) => {
-    if (error) return callback(error, null); //if error, error
-    if (response.statusCode !== 200) { //if the response is anything but 200 (aka response ok)
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
-      return;
-    }
-    //store data in arr
-    const arr = JSON.parse(body).results
-    const recipes = []
-    //push titles of books listed in arr and push into books
-    for (let x = 0; x < arr.length; x++) {
-      recipes.push(arr[x].title);
-    }
-    callback(null, recipes);
-  });
+const amountOfResults = (userInput) => {
+
+async  function fetchBookResults() {
+    const fetchBook = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${userInput}+intitle:${userInput}`)
+    const totalResult = await fetchBook.json()
+    return totalResult.totalItems/75;
+  }
+async  function fetchRecipeResults() {
+    const fetchRecipe = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${userInput}&apiKey=${process.env.FOOD_API_KEY}`)
+    const totalResult = await fetchRecipe.json()
+    return totalResult.totalResults;
+  }
+async  function fetchMovieResults() {
+    const fetchBook = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${userInput}`)
+    const totalResult = await fetchBook.json()
+    return totalResult.total_results
+  }
+  const arr = [fetchBookResults(), fetchRecipeResults(), fetchMovieResults()]
+  let obj = {
+  }
+  Promise.all(arr).then((values) => {
+    return values;
+  })
+  .then((values) => {
+obj = {
+  book: values[0],
+  recipe: values[1],
+  movie: values[2]
+}
+})
+
+  return obj;
 };
 
-const  fetchBook = function(callback) {
-  request(`https://www.googleapis.com/books/v1/volumes?q=${test}`, (error, response, body) => {
-    if (error) return callback(error, null); //if error, error
-    if (response.statusCode !== 200) { //if the response is anything but 200 (aka response ok)
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
-      return;
-    }
-    //store data in arr
-    const arr = JSON.parse(body).items
-    const books = []
-    //push titles of books listed in arr and push into books
-    for (let x = 0; x < arr.length; x++) {
-      books.push(arr[x].volumeInfo.title);
-    }
-    callback(null, books);
-  });
-};
+console.log(amountOfResults("toy story"));
 
-
-
-module.exports = { fetchBook , fetchRecipe};
+ module.exports = amountOfResults;
