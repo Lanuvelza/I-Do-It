0fa54b2fb337f15cb76646ae076bd0b4883d2246
Lanuvelza <ianlam1221@hotmail.com>
2021-02-09 00:21:25 +0000

    routes for login

---
+++ routes/login.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router  = express.Router();
+
+
+module.exports = (db) => {
+
+  router.get("/", (req, res) => {
+    res.send("login page");
+  });
+
+
+  // get /login/:id
+  router.get("/:id", (req, res) => {
+  req.session.user_id = req.params.id;
+  res.redirect('/dash');
+  console.log(req.session.user_id);
+  });
+  return router;
+
+};
+
+
